1d1758e9a20d9567df788a1f2b26681835f3ceae
Janani <janani@github.com>
2018-03-13 09:26:12 -0400

    Completed Application using Thymeleaf templates!

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,8 +1,13 @@
 package org.launchcode.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.launchcode.model.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by LaunchCode
@@ -10,16 +15,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Hello World";
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String helloForm() {
+		return "helloForm";
+	}
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public String hello(HttpServletRequest request, Model model) {
+    			
+    			String name = request.getParameter("name");
+    			/*
+    			if(name == null || name == "") {
+    				name = "world";
+    			}
+    			*/
+    			model.addAttribute("name", name);
+    			return "hello";
     }
-
-    @RequestMapping(value = "goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Goodbye";
-    }
-
 }
---
+++ src/main/java/org/launchcode/model/HelloMessage.java
@@ -0,0 +1,8 @@
+package org.launchcode.model;
+
+public class HelloMessage {
+	public static String getMessage(String name) {
+		return "Hello, " + name + "!";
+	
+	}
+}
